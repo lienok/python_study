e15111f3b5893514b31a83eef89bda37d5e47898
lenka.weiss <lienok@gmail.com>
2016-01-19 22:41:52 +0100

    Learn printing and improve reading command line inputs

--- 01.convert.py
+++ 01.convert.py
@@ -1,12 +1,9 @@
-import sys
+from sys import argv, exit  # inputs on the command line access via argv
 
-#Read inputs from user. 
-#Expects to have on 1.index real number and on 2.index the unit which from which you convert (either "inch" or "cm")
-if len(sys.argv) < 3:
-	sys.exit("Error, missing inputs! Expected number and units. Example: 3 cm or 4 inch")
+script, value, units = argv  #set those as expected arguments!
 
-value = float(sys.argv[1])
-units = sys.argv[2]
+value = float(argv[1])  #convert input to float number
+units = argv[2]
 
 #constants to use
 cm = "cm"
@@ -24,13 +21,13 @@ elif (units.lower().strip() == inch) :
 	unitsEntered = inch
 	unitsReturn = cm
 else :
-	sys.exit("Wrong inputs! Expected number and units. Example: 3 cm or 4 inch !")
+	exit("Wrong inputs! Expected number and units. Example: 3 cm or 4 inch !")
 
 print
 print ("You want to convert %s %s ?") % (value, units)
-answer = raw_input("Enter yes or y to continue: ") 
+answer = raw_input("Enter yes or y to continue: ")   #ask for input
 if not answer.startswith("y") :
-	sys.exit("You choose TO NOT continue. We will not convert it for you! ")
+	exit("You choose TO NOT continue. We will not convert it for you! ")
 	
 # make conversion according to the enterd value
 if (unitsEntered == cm):	
